83851db5bc49dd0ae75f5af8da48e49afd0ba7da
narzullayev0772 <alishernarzullayev7777@gmail.com>
2023-03-15 21:59:09 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/amiko", {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
